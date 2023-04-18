bce4245021f67c4f63f502b1374d27332d1b54bf
Mahmoud <mahmodhamidi12@gmail.com>
2023-04-18 16:24:32 +0300

    The First Commit in Project

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,59 @@
+public class DoublyLinkedList<T> {
+    private  class  Node<T>{
+        Node next;
+        Node previous;
+        T item;
+        public  Node(T newItem){
+            item=newItem;
+        }// end counstractor
+    }// end class Node
+    Node head;
+    Node tail;
+    int size=0;
+    public DoublyLinkedList(){
+
+    }// counstractor
+
+    public  void add_haed(T item){
+        Node new_node=new Node<>(item);
+        if (head==null){
+            head=tail=new_node;
+        }// end if
+        else {
+            new_node.next=head;
+            head.previous=new_node;
+            head=new_node;
+        }// end else
+        size++;
+    }// end function add_haed()
+
+
+    public void add_tail(T item){
+        Node new_node=new Node<>(item);
+        if (tail==null){
+            tail=head=new_node;
+        }// end if
+        else {
+            new_node.previous=tail;
+            tail.next=new_node;
+            tail=new_node;
+        }//end else
+    }// end function add_tail()
+
+    public void print_data_head(){
+        Node current=head;
+        while (current!=null){
+            System.out.print(" "+current.item);
+            current=current.next;
+        }//end while loop
+    }// end function print_data()
+
+    public void print_data_tail(){
+        Node current=tail;
+        while (current!=null){
+            System.out.print(" "+current.item);
+            current=current.previous;
+        }// end while loop
+    }// end function print_data_tail ()
+
+}// end class DoublyLinkedList
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        DoublyLinkedList<Integer> dl=new DoublyLinkedList<>();
+        dl.add_haed(10);
+        dl.add_haed(20);
+        dl.add_haed(30);
+        dl.add_haed(40);
+        dl.add_tail(50);
+        dl.print_data_head();
+        dl.clear();
+    }// end function main ()
+}// end class Main
